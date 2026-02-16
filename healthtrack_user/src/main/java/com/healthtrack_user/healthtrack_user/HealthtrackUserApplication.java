49435bb137420ad8e7488f1428a67600a78fd85c
alexiasandor <sandor.alexia16@gmail.com>
2026-02-16 13:27:47 +0200

    Initial  project setup

---
+++ healthtrack_user/src/main/java/com/healthtrack_user/healthtrack_user/HealthtrackUserApplication.java
@@ -0,0 +1,13 @@
+package com.healthtrack_user.healthtrack_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtrackUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthtrackUserApplication.class, args);
+	}
+
+}
