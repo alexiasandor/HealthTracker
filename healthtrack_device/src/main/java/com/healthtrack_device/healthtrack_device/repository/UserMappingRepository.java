234e6f3c45188d6df1c7cb3e159ee7f1b5fb86d2
alexiasandor <sandor.alexia16@gmail.com>
2026-03-23 10:55:15 +0200

    Revert "Merge pull request #3 from alexiasandor/feature-backend-device"
    This reverts commit 47f327db933540e57ba5099d0ca50e3cba8de685, reversing
    changes made to f262b48162ca682357308f5c72f3d3bb3a148848.

--- healthtrack_device/src/main/java/com/healthtrack_device/healthtrack_device/repository/UserMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthtrack_device.healthtrack_device.repository;
-
-import com.healthtrack_device.healthtrack_device.entity.UserMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserMappingRepository extends JpaRepository <UserMapping, UUID>{
-}
